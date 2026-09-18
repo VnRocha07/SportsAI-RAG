a2d693f61052864999d228a76a687e3bdf426bd3
Rochasar <vnrocha07@gmail.com>
2026-09-18 12:23:45 -0300

    First step of refactoring:

--- src/main.py
+++ src/main.py
@@ -19,47 +19,49 @@ Responda APENAS com base nas informações acima:
 
 
 def perguntar():
-    modeloCHOOSER = input("Selecione o modelo: \n 1-Pro \n 2-Flash \n")
-    if modeloCHOOSER == "1":
-        modelo = "deepseek-v4-pro"
-    elif modeloCHOOSER == "2":
-        modelo = "deepseek-v4-flash"
-    else:
-        print("Operação inválida, selecione um modelo.")
-        return
+    escolher_modelo = input("Selecione o modelo: \n 1-Pro \n 2-Flash \n")
 
-    pergunta = input("Pergunta: ")
+    modelos = {"1": "deepseek-v4-pro", "2": "deepseek-v4-flash"}
 
-    funcao_embeddings = HuggingFaceEmbeddings(
-        model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2",
-        encode_kwargs={
-            "normalize_embeddings": True
-        }
-    )
+    modelo = modelos.get(escolher_modelo, lambda: print(
+        "Operação inválida, selecione um modelo."))
 
-    db = Chroma(
-        persist_directory=CAMINHO_DB,
-        embedding_function=funcao_embeddings
-    )
+    while True: #Fiz só esse while true pra conversa ficar continua, depois do refactor vou add essa feature
+        pergunta = input("Pergunta: ")
+        if pergunta.lower().strip() == "sair":
+            print("Encerrando o programa.")
+            break
 
-    resultados = db.similarity_search_with_relevance_scores(pergunta, k=5)
-    if len(resultados) == 0 or resultados[0][1] < 0.2:
-        print("Não consegui encontrar nenhuma informação relevante na base")
-        return
+        funcao_embeddings = HuggingFaceEmbeddings(
+            model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2",
+            encode_kwargs={
+                "normalize_embeddings": True
+            }
+        )
 
-    textos_resultado = []
-    for resultado in resultados:
-        texto = resultado[0].page_content
-        textos_resultado.append(texto)
+        db = Chroma(
+            persist_directory=CAMINHO_DB,
+            embedding_function=funcao_embeddings
+        )
 
-    base_conhecimento = "\n\n----\n\n".join(textos_resultado)
-    prompt = PromptTemplate.from_template(prompt_template)
-    prompt_formatado = prompt.invoke({
-        "pergunta": pergunta,
-        "base_conhecimento": base_conhecimento
-    })
+        resultados = db.similarity_search_with_relevance_scores(pergunta, k=7)
+        if len(resultados) == 0 or resultados[0][1] < 0.2:
+            print("Não consegui encontrar nenhuma informação relevante na base")
+            return
 
-    perguntar_deepseek(prompt_formatado.text, modelo)
+        textos_resultado = []
+        for resultado in resultados:
+            texto = resultado[0].page_content
+            textos_resultado.append(texto)
+
+        base_conhecimento = "\n\n----\n\n".join(textos_resultado)
+        prompt = PromptTemplate.from_template(prompt_template)
+        prompt_formatado = prompt.invoke({
+            "pergunta": pergunta,
+            "base_conhecimento": base_conhecimento
+        })
+
+        perguntar_deepseek(prompt_formatado.text, modelo)
 
 
 perguntar()
